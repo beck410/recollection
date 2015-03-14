4b9258ba42a23655540ee5ba7033bc6f900137c0
beck410 <beck410@hotmail.com>
2015-03-14 10:52:36 -0500

    initial asp.net net mvc setup

---
+++ recollection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(recollection.Startup))]
+namespace recollection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
