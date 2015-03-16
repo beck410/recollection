7f718bfeb3005f18fff6719447419f928e80150d
beck410 <beck410@hotmail.com>
2015-03-16 11:10:29 -0500

    added new model - place

---
+++ recollection/Models/Place.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace recollection.Models {
+  public class Place {
+    public int ID { get; set; }
+    public string UserID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+
+    public ICollection<Memory> Memories { get; set; }
+    public ICollection<Note> Notes { get; set; }
+    public ICollection<Image> Images { get; set; }
+  }
+}
