69cae0722e4cf5178bf3c60a10b3e78cae67aa3f
beck410 <beck410@hotmail.com>
2015-03-17 19:13:39 -0500

    changed memory ctor with passing tests

--- recollection.tests/UnitTests/MemoryModelTests.cs
+++ recollection.tests/UnitTests/MemoryModelTests.cs
@@ -6,9 +6,19 @@ namespace recollection.tests.UnitTests {
   [TestClass]
   public class MemoryModelTests {
     [TestMethod]
-    public void MemoryConstructorTestSuccess() {
-      Memory memory = new Memory("June and i met at this beach");
+    public void MemoryConstructorTestForLocationSuccess() {
+      Memory memory = new Memory("June and i met at this beach",false,1);
       Assert.AreEqual("June and i met at this beach", memory.Message);
+      Assert.AreEqual(0, memory.PersId);
+      Assert.AreEqual(1, memory.LocationID);
+    }
+
+    [TestMethod]
+    public void MemoryConstructorTestForPersonSuccess() {
+      Memory memory = new Memory("June and i have been married for 20 years",true,1);
+      Assert.AreEqual("June and i have been married for 20 years", memory.Message);
+      Assert.AreEqual(1, memory.PersId);
+      Assert.AreEqual(0, memory.LocationID);
     }
   }
 }
--- recollection/Models/Memory.cs
+++ recollection/Models/Memory.cs
@@ -11,8 +11,13 @@ namespace recollection.Models {
     public int LocationID { get; set; }
     public string Message { get; set; }
 
-    public Memory(string message) {
+    public Memory(string message, bool isPerson ,int objId) {
       this.Message = message;
+      if (isPerson) {
+        this.PersId = objId;
+      } else {
+        this.LocationID = objId;
+      }
     }
 
     public Memory() {
