1f94d1c420fbc4947c299fc53c87782bb9a211a0
beck410 <beck410@hotmail.com>
2015-03-18 20:50:24 -0500

    api person put and delete requests working

--- recollection/Controllers/RecollectionController.cs
+++ recollection/Controllers/RecollectionController.cs
@@ -33,5 +33,29 @@ namespace recollection.Controllers
         person_repo.Add(person);
         return new HttpResponseMessage(HttpStatusCode.OK);
       }
+
+      [HttpGet]
+      [Route("Persons/{userID}/{id}")]
+      public System.Web.Mvc.JsonResult SinglePerson(string userID, int id) {
+        var person = person_repo.GetById(id);
+        var json = new System.Web.Mvc.JsonResult();
+        json.Data = new { person };
+        return json;
+      }
+
+      [HttpPut]
+      [Route("Persons/{userID}/Update/{id}")]
+      public string UpdatePerson([FromBody] Person person) {
+        person_repo.Edit(person);
+        var name = person_repo.All()[2].Name;
+        return name;
+      }
+
+      [HttpDelete]
+      [Route("Persons/{userID}/Delete/{id}")]
+      public HttpResponseMessage DeletePerson(int id) {
+        person_repo.Delete(id);
+        return new HttpResponseMessage(HttpStatusCode.OK);
+      }
     }
 }
