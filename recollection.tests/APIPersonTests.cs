4197dfe0064726b75b8885248af9739bb4b16bb1
beck410 <beck410@hotmail.com>
2015-03-19 10:06:50 -0500

    api test reference - WIP

---
+++ recollection.tests/APIPersonTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using recollection.Controllers;
+using System.Web;
+
+namespace recollection.tests.APITests {
+  [TestClass]
+  public class APIPersonTests {
+
+    [TestMethod]
+    public void APITestGetPersons() {
+      var controller = new RecollectionController("Name=recollectionTest");
+
+      var result = controller.GetPersons("04f7c20a-e584-4b7f-8d28-a88e8f1b4ac6");
+      Assert.AreEqual(2, result.Count);
+    }
+  }
+}
