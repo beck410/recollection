65b63e66e32e8ead231430264fec51b3b78a62dd
beck410 <beck410@hotmail.com>
2015-03-19 11:11:35 -0500

    changed layout and added app controller/view

---
+++ recollection/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace recollection.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
