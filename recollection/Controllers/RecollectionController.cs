b98aab5ea580ad64076ee53993c67e63a4a8531b
beck410 <beck410@hotmail.com>
2015-03-18 19:40:24 -0500

    api person[Post] working

--- recollection/Controllers/RecollectionController.cs
+++ recollection/Controllers/RecollectionController.cs
@@ -9,12 +9,14 @@ using System.Web.Http;
 
 namespace recollection.Controllers
 {
+    [RoutePrefix("api/Recollection")]
     public class RecollectionController : ApiController
     {
       private PersonRepo person_repo = new PersonRepo();
 
-      // GET: /api/Persons/userID
+      // GET: /api/Recollection/Persons/userID
       [HttpGet]
+      [Route("Persons/{userID}")]
       public System.Web.Mvc.JsonResult Persons(string userID) {
         //return person_repo.GetAllByUserId(userID);
         var people = person_repo.GetAllByUserId(userID);
@@ -24,9 +26,12 @@ namespace recollection.Controllers
       }
 
       //POST: api/Persons/userID
-      //public HttpResponseMessage Persons(string userID, Person person) {
-      //  person_repo.Add(person);
-      //  return new HttpResponseMessage(HttpStatusCode.OK);
-      //}
+
+      [HttpPost]
+      [Route("Persons/{userID}")]
+      public HttpResponseMessage Persons(string userID, [FromBody] Person person) {
+        person_repo.Add(person);
+        return new HttpResponseMessage(HttpStatusCode.OK);
+      }
     }
 }
