945d8f3a3a684c2c596b981f5afd9d427f995492
beck410 <beck410@hotmail.com>
2015-03-19 12:44:01 -0500

    changed app folder to angular - route conflicts

---
+++ recollection/Angular/js/config/dashboard.config.js
@@ -0,0 +1,10 @@
+﻿; (function () {
+    'use strict';
+    angular.module('recollection')
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: '/Angular/recollection.html'
+        })
+    })
+})();
