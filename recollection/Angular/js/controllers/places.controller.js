e10545b834ecfb99950f743a3616c0aee4a72f7a
beck410 <beck410@hotmail.com>
2015-03-24 18:08:56 -0500

    add new place working on place page

--- recollection/Angular/js/controllers/places.controller.js
+++ recollection/Angular/js/controllers/places.controller.js
@@ -1,14 +1,47 @@
 ﻿;(function() {
     'use strict';
     angular.module('recollection')
-    .controller('placesController', function ($routeParams, apiPlace) {
+    .controller('placesController', function ($routeParams, USERID, apiPlace,$scope,uploadImage) {
         var vm = this;
+        vm.newPlace = { UserID: USERID }
 
 
 
         apiPlace.getPlaces(function(places) {
             vm.places = places;
         })
+
+        vm.fileSelected = function (event) {
+            uploadImage.setThumbnail(vm.files[0], function (fileName,base64) {
+                vm.fileName = fileName;
+                vm.files[0].dataUrl = base64;
+                $scope.$apply();
+            })
+        }
+
+        vm.clearForm = function () {
+            vm.newPlace = { UserID: USERID };
+            vm.files = null;
+            vm.fileName = null;
+        }
+
+        vm.addNewPlace = function () {
+            if (vm.files) {
+                uploadImage.uploadToS3(vm.files, USERID, vm.fileName, function (fileLink) {
+                    vm.newPlace.MainImage = fileLink;
+                    apiPlace.postPlace(vm.newPlace, function () {
+                        vm.newPlace = { UserID: USERID }
+                        vm.files = null;
+                        vm.fileName = null;
+                        apiPlace.getPlaces(function (places) {
+                            vm.places = places;
+                        })
+                    })
+                })
+            } else {
+                console.log('no image added');
+            }
+        }
 
         //var place = {
         //    "ID": 6,
